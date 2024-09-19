d9bf62cbc2faba9e3181d366bce8a7616ce59503
Tim <81175852+wharvex@users.noreply.github.com>
2024-09-19 15:23:27 -0400

    first commit

---
+++ Armoire/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Armoire.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
