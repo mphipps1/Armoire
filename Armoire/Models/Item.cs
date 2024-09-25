1b30507f7e1f5617435463c19f230a3124fbea7f
Bret-1 <bretjempty@gmail.com>
2024-09-25 13:17:08 -0400

    Adding a new view for adding new items or drawers and a basic item class

---
+++ Armoire/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armoire.Models
+{
+    public class Item
+    {
+        public string path { get; set; }
+
+        public void Execute()
+        {
+            System.Diagnostics.Process.Start(path);
+        }
+    }
+}
