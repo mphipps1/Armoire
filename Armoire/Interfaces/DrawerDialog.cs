fffab71f370d19fc81c48121703053cc87bcce5e
Akhil Nair <akhiln3011@gmail.com>
2024-09-29 13:04:51 -0400

    Created a sample drawer dialog view

---
+++ Armoire/Interfaces/DrawerDialog.cs
@@ -0,0 +1,43 @@
+﻿using Armoire.Views;
+using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armoire.Interfaces
+{
+    public interface IDrawerDialogService
+    {
+        public void DrawerDialogView();
+
+    }
+    public class DrawerDialog : IDrawerDialogService
+    {
+        public DrawerDialog() { 
+        }
+
+        public void DrawerDialogView()
+        {
+            throw new NotImplementedException();
+        }
+
+        /**
+public void DrawerDialogView()
+{
+   DrawerDialogView dialog = new DrawerDialogView();
+
+   var Windodialog = new Window
+   {
+       Content = dialog,
+       Width = 300,
+       Height = 200
+   };
+
+   Windodialog.ShowDialog(this);
+}
+*/
+    }
+}
--- Armoire/Views/DrawerDialogView.axaml.cs
+++ Armoire/Views/DrawerDialogView.axaml.cs
@@ -1,13 +1,48 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Armoire.Views;
 
 public partial class DrawerDialogView : UserControl
 {
+    private bool isDrawerOpen = false;
     public DrawerDialogView()
     {
         InitializeComponent();
+    }
+
+
+    private void Btn_PointerReleased(object sender, PointerReleasedEventArgs e)
+    {
+        if (e.InitialPressMouseButton == MouseButton.Right)
+        {
+            var button = sender as Button;
+            button?.ContextMenu?.Open(button);
+        }
+    }
+
+    public void ToggleDrawer()
+    {
+        var drawer = this.FindControl<Border>("Drawer");
+        drawer.IsVisible = true;
+        drawer.Height = 100;
+
+        if (isDrawerOpen)
+        {
+            drawer.IsVisible = false;
+            drawer.Height = 0;
+            drawer.Width = 0;
+        }
+        else
+        {
+            drawer.IsVisible = true;
+            drawer.Height = 300;
+            drawer.Width = 500;
+        }
+
+
+        isDrawerOpen = !isDrawerOpen;
     }
 }
