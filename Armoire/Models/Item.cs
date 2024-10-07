9f5f1187d3348185f0c27020909ce00b3c10d91e
Bret-1 <131408351+Bret-1@users.noreply.github.com>
2024-10-06 21:10:04 -0400

    add drawer button now has a view that accepts a name and path, works with C:\Users\Queee\AppData\Roaming\Clone Hero Launcher\gameFiles\Clone Hero.exe on my system, but probably needs further testing. Haven't implemented the ability to add a drawer yet

--- Armoire/Models/Item.cs
+++ Armoire/Models/Item.cs
@@ -4,27 +4,36 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using Armoire.Interfaces;
+using Material.Dialog.ViewModels;
 
 namespace Armoire.Models
 {
     public class Item : IContentsUnit
     {
-        public string Path { get; set; }
+        private Process process {  get; set; }
+        public string ParentDrawer { get; set; }
+        public string Name { get; set; }
+        public string? IconPath { get; set; }
 
         public void Execute()
         {
-            System.Diagnostics.Process.Start(Path);
+            process.Start();
         }
 
         public Item(string name, string path, string parentDrawer)
         {
             Name = name;
-            Path = path;
+            //make a new process out of the path
+            //the old method gave an error when accessing other folders
+            //this method also doesnt require proper quoting around folders or the executable name if it has a space in it
+            //System.Diagnostics.Process.Start(path);
+            process = new Process();
+            process.StartInfo.FileName = path;
+            process.StartInfo.UseShellExecute = true;
             ParentDrawer = parentDrawer;
         }
-        public string ParentDrawer { get; set; }
-        public string Name { get; set; }
-        public string? IconPath { get; set; }
+
     }
 }
