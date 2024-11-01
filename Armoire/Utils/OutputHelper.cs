b8157f587d464e839c2056979aa72e27d4941983
Tim <81175852+wharvex@users.noreply.github.com>
2024-10-31 21:06:19 -0400

    add OutputHelper

---
+++ Armoire/Utils/OutputHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Armoire.Utils;
+
+public class OutputHelper
+{
+    public static string DocPath { get; } =
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+    public static void DebugPrintJson(object obj, string suffix)
+    {
+        var jSets = new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            Converters = [new StringEnumConverter()],
+            Formatting = Formatting.Indented,
+            TypeNameHandling = TypeNameHandling.All
+        };
+        DebugPrintJsonOutput(JsonConvert.SerializeObject(obj, jSets), suffix);
+    }
+
+    public static void DebugPrintJsonOutput(string output, string suffix)
+    {
+        using var outputFile = new StreamWriter(
+            Path.Combine(DocPath, "ArmoireDebugOutput_" + suffix + ".json")
+        );
+        outputFile.WriteLine(output);
+    }
+}
