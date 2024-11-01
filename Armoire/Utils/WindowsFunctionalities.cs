f3773542592af72f60f964fe5c02fb55d27f3a7a
Bret-1 <bretjempty@gmail.com>
2024-11-01 18:54:59 -0400

    adding button with flyout to dock that holds buttons to logout, restart and shutdown. tested on windows 10, still needs testing on windows 11

---
+++ Armoire/Utils/WindowsFunctionalities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armoire.Utils
+{
+    public static class WindowsFunctionalities
+    {
+        //https://stackoverflow.com/questions/462381/restarting-windows-from-within-a-net-application
+        public static void Restart()
+        {
+            StartShutDown("-f -r -t 5");
+        }
+
+        public static void LogOff()
+        {
+            StartShutDown("-l");
+        }
+
+        public static void Shutdown()
+        {
+            StartShutDown("-f -s -t 5");
+        }
+
+        private static void StartShutDown(string param)
+        {
+            ProcessStartInfo proc = new ProcessStartInfo();
+            proc.FileName = "cmd";
+            proc.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Arguments = "/C shutdown " + param;
+            Process.Start(proc);
+        }
+    }
+}
