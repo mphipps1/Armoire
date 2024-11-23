54b5fc530a0a69a6eeeba02ecb3af2ea21156785
Akhil Nair <akhiln3011@gmail.com>
2024-11-22 19:52:56 -0500

    Jumplist viemodel is added

---
+++ Armoire/ViewModels/JumpListViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armoire.ViewModels
+{
+    public class JumpListViewModel
+    {
+        private List<FileInfo> fileInfos = new List<FileInfo>();
+
+        public static List<ItemViewModel> Items = new List<ItemViewModel>();
+
+        public JumpListViewModel()
+        {
+
+
+
+        }
+
+
+
+
+        public void GetJumpList()
+        {
+            string userName = Environment.UserName;
+            string directloc = $"C:\\Users\\{userName}\\AppData\\Roaming\\Microsoft\\Windows\\Recent";
+
+            // files list from the root directory and prints it
+            string[] fyles = Directory.GetFiles(directloc);
+
+            for (int i = 0; i < fyles.Length; i++)
+            {
+
+                fileInfos.Add(new FileInfo(fyles[i]));
+                Type shellObjectType = Type.GetTypeFromProgID("WScript.Shell");
+                dynamic windowsShell = Activator.CreateInstance(shellObjectType);
+                // dynamic shortcut = windowsShell.CreateShortcut(fileInfos.ElementAt(i).FullName);
+
+                //   var file = shortcut.TargetPath;
+                string applicationName = Path.GetFileNameWithoutExtension(fileInfos.ElementAt(i).FullName);
+            }
+
+
+        }
+
+
+    }
+}
--- Armoire/Views/ItemView.axaml.cs
+++ Armoire/Views/ItemView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Armoire.Views;
@@ -9,5 +10,21 @@ public partial class ItemView : UserControl
     public ItemView()
     {
         InitializeComponent();
+    }
+
+
+    public void ButtonPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(null).Properties.PointerUpdateKind == PointerUpdateKind.RightButtonPressed)
+        {
+            if (sender is Button button)
+            {
+
+                if (button.ContextMenu != null)
+                {
+                    button.ContextMenu.Open(button);
+                }
+            }
+        }
     }
 }
