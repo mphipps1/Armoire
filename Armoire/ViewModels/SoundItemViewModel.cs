dcfafdafb5ba0448d5d14ee262c45728d3ac3aa6
jimmy <brooksjames1212@gmail.com>
2026-03-19 13:30:06 -0400

    Added cross platform solution for sound settings.

--- Armoire/ViewModels/SoundItemViewModel.cs
+++ Armoire/ViewModels/SoundItemViewModel.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,7 +35,34 @@ namespace Armoire.ViewModels
         public override void HandleContentsClick()
         {
             // Launches windows settings to the sound page
-            Process.Start(new ProcessStartInfo("ms-settings:sound") { UseShellExecute = true });
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process.Start(new ProcessStartInfo("ms-settings:sound") { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start(new ProcessStartInfo("open", "x-apple.systempreferences:com.apple.preference.sound") { UseShellExecute = false });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                // Linux: Try common sound settings commands in order of preference
+               if(!TryStartProcess("gnome-control-center", "sound"))
+                   if(!TryStartProcess("pavucontrol"))
+                       if(!TryStartProcess("xfce4-mixer"))
+                           TryStartProcess("kcmshell5", "kcm_pulseaudio");
+            }
+        }
+        private bool TryStartProcess(string fileName, string arguments = "")
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(fileName, arguments) { UseShellExecute = true });
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
