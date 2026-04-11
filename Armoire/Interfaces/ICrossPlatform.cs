200837600d72bf135b37952451cb0153f85bbcb2
MidnightStormz0 <brooksjames1212@gmail.com>
2026-04-10 21:50:29 -0400

    added RunningApplicationInfo class to ICrossPlatform.

--- Armoire/Interfaces/ICrossPlatform.cs
+++ Armoire/Interfaces/ICrossPlatform.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+
 namespace Armoire.Interfaces;
 
 public interface ICrossPlatform
@@ -6,6 +10,15 @@ public interface ICrossPlatform
     {
         public float Latitude { get; set; }
         public float Longitude { get; set; }
+    }
+
+    public class RunningApplicationInfo
+    {
+        public required string StableId { get; set; }
+        public required int ProcessId { get; set; }
+        public required string DisplayName { get; set; }
+        public string? BundleIdentifier { get; set; }
+        public required string? ApplicationPath { get; set; }
     }
     
     bool IsOnBattery();
@@ -16,6 +29,12 @@ public interface ICrossPlatform
     void Restart();
     void LogOff();
     void Shutdown();
+    IReadOnlyList<RunningApplicationInfo> GetRunningApplications();
+    Task BringApplicationToForegroundAsync(int processId);
+    Task CloseApplicationAsync(int processId);
+    
+    Bitmap GetAppIcon(string appID);
+    
     
     public static ICrossPlatform Instance
     {
